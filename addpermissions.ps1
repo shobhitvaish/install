896cb82a1e4f1df2fa2ab417e3e3a5dbedf6f8f5
Christoph <christoph.hannebauer@glueckkanja-gab.com>
2021-10-18 15:22:32 +0200

    No Install-Module

--- addpermissions.ps1
+++ addpermissions.ps1
@@ -4,12 +4,13 @@ param(
     $WebAppName
 )
 
-Install-Module AzureAD
+#Install-Module AzureAD
 #$TenantId="<TENANT-ID>"
 #$ResourceGroup = "{SCEPMAN AZURE WEB APP RESOURCE GROUP}"
 #$WebAppName="{SCEPMAN AZURE WEB APP NAME}"
 $ServicePrincipalId = (Get-AzWebApp -ResourceGroupName $ResourceGroup -Name $WebAppName).Identity.PrincipalId
 $PermissionName = "SCEP_CHALLENGE_PROVIDER"
+
 Connect-AzureAd -TenantId $TenantId
 $IntuneServicePrincipal = Get-AzureAdServicePrincipal -SearchString "Microsoft Intune API" | Select-Object First 1
 $AppRole = $IntuneServicePrincipal.AppRoles | Where-Object {$_.Value -eq $PermissionName -and $_.AllowedMemberTypes -contains "Application"}
